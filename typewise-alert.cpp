5f1d2a08a8039aba6cd80a0e1d71968649d42b6e
sumanthsags <120707040+sumanthsags@users.noreply.github.com>
2024-08-26 20:50:40 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -11,24 +11,15 @@ BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
   return NORMAL;
 }
 
-BreachType classifyTemperatureBreach(
-    CoolingType coolingType, double temperatureInC) {
-  int lowerLimit = 0;
-  int upperLimit = 0;
-  switch(coolingType) {
-    case PASSIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 35;
-      break;
-    case HI_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 45;
-      break;
-    case MED_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 40;
-      break;
-  }
+const coolingLimits = {
+  PASSIVE_COOLING: { lowerLimit: 0, upperLimit: 35 },
+  HI_ACTIVE_COOLING: { lowerLimit: 0, upperLimit: 45 },
+  MED_ACTIVE_COOLING: { lowerLimit: 0, upperLimit: 40 }
+};
+
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) 
+{
+  const { lowerLimit, upperLimit } = coolingLimits[coolingType];
   return inferBreach(temperatureInC, lowerLimit, upperLimit);
 }
 
