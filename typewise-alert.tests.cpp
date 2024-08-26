d4c97189fd45a65810e330c1a7d79972fbb747df
sumanthsags <120707040+sumanthsags@users.noreply.github.com>
2024-08-26 23:28:27 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -6,16 +6,23 @@ TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
 
 }
 
-TEST(TypeWiseAlertTestSuite,PassiveCoolingHighTemp) 
+TEST(TypeWiseAlertTestSuite,PassiveCoolingHighTempToController) 
 {
   BatteryCharacter batteryChar1;
   batteryChar1.coolingType = PASSIVE_COOLING;
   checkAndAlert(TO_CONTROLLER,batteryChar1,38);
 }
 
-TEST(TypeWiseAlertTestSuite,PassiveCoolingLowTemp) 
+TEST(TypeWiseAlertTestSuite,PassiveCoolingLowTempToEmail) 
 {
   BatteryCharacter batteryChar1;
   batteryChar1.coolingType = PASSIVE_COOLING;
   checkAndAlert(TO_EMAIL,batteryChar1,-1);
+}
+
+TEST(TypeWiseAlertTestSuite,PassiveCoolingHighTempToEmail) 
+{
+  BatteryCharacter batteryChar1;
+  batteryChar1.coolingType = PASSIVE_COOLING;
+  checkAndAlert(TO_EMAIL,batteryChar1,38);
 }
