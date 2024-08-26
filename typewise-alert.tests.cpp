c9daaeaa754e0de707b8a4afcb91311b00426b5b
sumanthsags <120707040+sumanthsags@users.noreply.github.com>
2024-08-26 23:23:43 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -12,3 +12,10 @@ TEST(TypeWiseAlertTestSuite,PassiveCoolingHighTemp)
   batteryChar1.coolingType = PASSIVE_COOLING;
   checkAndAlert(TO_CONTROLLER,batteryChar1,38);
 }
+
+TEST(TypeWiseAlertTestSuite,PassiveCoolingLowTemp) 
+{
+  BatteryCharacter batteryChar1;
+  batteryChar1.coolingType = PASSIVE_COOLING;
+  checkAndAlert(TO_EMAIL,batteryChar1,-1);
+}
